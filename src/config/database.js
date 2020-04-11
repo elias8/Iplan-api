9e71b13c335984508a7be2f60e09e0818eacf42c
Elias Andualem <eliasandualem8@gmail.com>
2020-04-11 13:10:57 +0300

    changed file name

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,26 @@
 'use strict';
 
 const log = require('./logger');
-const mongoose = require('mongoose');
+const database = require('mongoose');
 const mongooseHidden = require('mongoose-hidden');
 
 function makeDatabase() {
     return Object.freeze({connect});
 
     async function connect(url) {
-        if (process.env.NODE_ENV === 'development') mongoose.set('debug', true);
+        if (process.env.NODE_ENV === 'development') database.set('debug', true);
         const mongooseOptions = {keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true};
         const mongooseHiddenOption = {defaultHidden: {'_id': false, password: true, '__v': true}};
         const hiddenPlugin = mongooseHidden(mongooseHiddenOption);
 
-        mongoose.Promise = Promise;
-        mongoose.set('useCreateIndex', true);
-        mongoose.plugin(hiddenPlugin);
-        mongoose.connection.on('connected', () => log.i('Database Connected.'));
+        database.Promise = Promise;
+        database.set('useCreateIndex', true);
+        database.plugin(hiddenPlugin);
+        database.connection.on('connected', () => log.i('Database Connected.'));
 
         try {
-            await mongoose.connect(url, mongooseOptions);
-            return mongoose.connection;
+            await database.connect(url, mongooseOptions);
+            return database.connection;
         } catch (e) {
             log.e('Unable to connect the database. Please check the database is up and running. Or the connection string.');
             return false;
