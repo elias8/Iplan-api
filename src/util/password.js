424358420a85a4ab8a5e271f5fa04617c8478d57
Elias Andualem <eliasandualem8@gmail.com>
2020-04-26 19:04:48 +0300

    added eslint

--- src/util/password.js
+++ src/util/password.js
@@ -3,16 +3,16 @@ const bcrypt = require('bcrypt-nodejs');
 const WORK_FACTOR = process.env.SALT_WORK_FACTOR;
 
 const genSalt = async () => {
-    return bcrypt.genSaltSync(WORK_FACTOR);
+	return bcrypt.genSaltSync(WORK_FACTOR);
 };
 
 exports.hasPassword = async (password) => {
-    const salt = await genSalt();
-    return bcrypt.hashSync(password, salt);
+	const salt = await genSalt();
+	return bcrypt.hashSync(password, salt);
 };
 
 exports.comparePassword = async (password, encryptedPassword) => {
-    return await bcrypt.compareSync(password, encryptedPassword);
+	return bcrypt.compareSync(password, encryptedPassword);
 };
 
 
